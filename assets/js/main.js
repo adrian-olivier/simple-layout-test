61d6dd7c0a6fd78c16f8dd3bfcd6b731d403645d
Adrian Olivier <adrianolivier.work@gmail.com>
2022-04-06 18:42:30 -0500

    fix: reduce banner size and update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,28 @@
 let slideIndex = 0;
+let nextTimer = null;
 showSlides();
 
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slideIndex++;
+
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000);
+  
+  nextTimer = setTimeout(showSlides, 2000);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  slideIndex = n;
+  clearTimeout(nextTimer);
+  showSlides();
 }
